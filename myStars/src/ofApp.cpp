47720175e107cbfe1fc4df68f2d12c2ce7ae13f9
Katie Smillie <katiesmillie@gmail.com>
2015-04-11 16:17:11 -0400

    good enough for now

--- myStars/src/ofApp.cpp
+++ myStars/src/ofApp.cpp
@@ -4,29 +4,22 @@
 void ofApp::setup(){
     
     ofBackground(0,0,0);
-    ofSetFrameRate(9);
+
     startingPoint.set(500,500);
     
     panel.setup("star", "settings.xml", 510, 10);
-    
-    
     panel.add(nPoints.set("nPoints", 6, 4, 50));
     panel.add(radius.set("radius", 200, 80, 400));
-    panel.add(nucleus.set("nucleus", 1, 2, 4));
-
+    (nucleus.set("nucleus", 3, 3, 3));
     panel.add(resample.set("resample", 4, 2, 10));
     panel.add(smoothing.set("smoothing", 20, 0, 50));
     panel.add(twist.set("twist", 0, 0, 9));
     panel.add(color.set("color",ofColor(100,200,200),ofColor(0,0),ofColor(255,255)));
-
-
     
 }
 
 //--------------------------------------------------------------
 void ofApp::drawStar(){
-    
-   
     
     float angleDivision = TWO_PI / (float)nPoints;
     float outerRadius = radius;
@@ -46,6 +39,26 @@ void ofApp::drawStar(){
         allPts.push_back(innerPt);
 
     }
+
+    ofPolyline line;
+    
+    for (int i = 0; i < nPoints * 2; i++) {
+        line.addVertex(allPts[i]);
+    }
+    
+    line.close();
+    
+    line = line.getResampledBySpacing(resample);
+    line = line.getSmoothed(smoothing);
+    
+    
+    ofBeginShape();
+    
+    for (int i = 0; i < line.size(); i++) {
+        ofVertex(line[i]);
+    }
+    
+    ofEndShape();
 }
 
 
@@ -58,29 +71,11 @@ void ofApp::update(){
 void ofApp::draw(){
     
     ofSetColor(color);
+    
     drawStar();
 
-    ofPolyline line;
+    panel.draw();
     
-        for (int i = 0; i < nPoints * 2; i++) {
-            line.addVertex(allPts[i]);
-        }
-    
-        line.close();
-    
-        line = line.getResampledBySpacing(resample);
-        line = line.getSmoothed(smoothing);
-
-    
-        ofBeginShape();
-    
-        for (int i = 0; i < line.size(); i++) {
-            ofVertex(line[i]);
-        }
-        
-        ofEndShape();
-    
-        panel.draw();
 }
 
 
@@ -104,7 +99,7 @@ void ofApp::mouseMoved(int x, int y ){
 void ofApp::mouseDragged(int x, int y, int button){
     
     startingPoint.set(x,y);
-    
+
 }
 
 //--------------------------------------------------------------
@@ -112,12 +107,11 @@ void ofApp::mousePressed(int x, int y, int button){
     
     startingPoint.set(x,y);
     
-    
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseReleased(int x, int y, int button){
-    
+
 }
 
 //--------------------------------------------------------------
